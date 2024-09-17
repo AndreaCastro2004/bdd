2d341bdf355f750dd6144ebe0a475fcf54b63b94
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-17 16:54:44 -0500

    Se agregan los 8 insert a la tabla estudiantes

--- inserciones/scripts/tabla_estudiantes.sql
+++ inserciones/scripts/tabla_estudiantes.sql
@@ -7,3 +7,32 @@ create table estudiantes(
 	fecha_nacimiento date not null,
 	constraint estudiantes_pk primary key(cedula)
 )
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567890','Maria','Zambrano','mariZam_ldu@.gmail.com','15/03/1978')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0987654321','Sofia','Barreto','sofiaBa_ldu@.gmail.com','17/04/1988')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567980','Rachel','Morales','RachelMO_ldu@.gmail.com','20/05/1998')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567789','Jose','Arturo','JoseAr_ldu@.gmail.com','15/03/2000')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('2234567890','Marcos','Benito','MarcoBen_ldu@.gmail.com','17/03/1978')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234577890','Rosio','Paredes','RocioPar_ldu@.gmail.com','15/03/1999')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567800','Monica','Zurita','MoricaZur_ldu@.gmail.com','15/03/2002')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234560890','Simiendo','Paredes','SimiendoPar_ldu@.gmail.com','17/04/2004')
+
+select * from estudiantes
