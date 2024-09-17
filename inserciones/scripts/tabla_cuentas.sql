5b496bed5e1fa7e80f6ffe032d5dfd78476da33f
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-17 16:37:51 -0500

    se ejecutan los 10 insert en la tabla cuentas

--- inserciones/scripts/tabla_cuentas.sql
+++ inserciones/scripts/tabla_cuentas.sql
@@ -6,3 +6,33 @@ create table cuentas(
 	saldo money not null,
 	constraint cuentas_pk primary key(numero_cuenta)
 )
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('12345','17181','17/09/2004',100)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('13345','11922','27/08/2004',150)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('11315','11111','16/05/2004',160)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('22223','23423','12/12/2005',170)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('12375','17161','17/03/2005',180)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('19845','97181','27/04/2005',190)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('86800','17181','30/05/2005',200)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('54321','54321','2/06/2005',210)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('67890','09342','10/06/2005',220)
+
+insert into cuentas(numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values('09876','23490','24/06/2005',230)
+
