2e1ebee3b929431e4ff0bb9a5231060ba69e8867
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-18 10:51:05 -0500

    Se crea carpeta actualizaciones con los scripts

---
+++ actualizaciones/scripts/tabla_estudiantes.sql
@@ -0,0 +1,38 @@
+--crear tabla estudiantes
+create table estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key(cedula)
+)
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567890','Maria','Zambrano','mariZam_ldu@.gmail.com','15/03/1978')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0987654321','Sofia','Barreto','sofiaBa_ldu@.gmail.com','17/04/1988')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567980','Rachel','Morales','RachelMO_ldu@.gmail.com','20/05/1998')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567789','Jose','Arturo','JoseAr_ldu@.gmail.com','15/03/2000')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('2234567890','Marcos','Benito','MarcoBen_ldu@.gmail.com','17/03/1978')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234577890','Rosio','Paredes','RocioPar_ldu@.gmail.com','15/03/1999')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567800','Monica','Zurita','MoricaZur_ldu@.gmail.com','15/03/2002')
+
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234560890','Simiendo','Paredes','SimiendoPar_ldu@.gmail.com','17/04/2004')
+
+select * from estudiantes
---
+++ actualizaciones/scripts/tabla_videojuegos.sql
@@ -0,0 +1,33 @@
+-- crear una tabla llamada videojuegos
+create videojuegos(
+	codigo int not null,
+	nombre varchar(100) not null,
+	descripcion varchar(300),
+	valoracion int not null,
+	constraint videojuegos_pk primary key(codigo)
+)
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('12345','Micki mause','gamer profecional con un IQ del 90',100)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('12355','TOKOSHIDA','Novato con ganas de aprender',12)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('12455','Marcie','Todo por 1',55)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('17455','Topodey','SI SE PUEDE MI GENTE',60)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('88888','Mayse','Todos contra todos',35)
+
+--insert solo con los campos obligatorios
+insert into videojuegos(codigo,nombre,valoracion)
+values('00005','Surigat',20)
+
+insert into videojuegos(codigo,nombre,valoracion)
+values('01115','Moret',69)
+
+insert into videojuegos(codigo,nombre,valoracion)
+values('02255','Contet',80)
+
