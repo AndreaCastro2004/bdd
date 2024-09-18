7b3775d0055983576ddee66051f8466cb168f3b4
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-18 11:10:36 -0500

    Se actualiza_apellido de tabla estudiantes

--- actualizaciones/scripts/tabla_estudiantes.sql
+++ actualizaciones/scripts/tabla_estudiantes.sql
@@ -7,6 +7,13 @@ create table estudiantes(
 	fecha_nacimiento date not null,
 	constraint estudiantes_pk primary key(cedula)
 )
+select * from estudiantes
+--actualizar apellido_hernandes a quienes empieza cedula 17
+update estudiantes set apellido='Hernandez'
+where cedula like '17%'
+
+
+
 insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
 values('1234567890','Maria','Zambrano','mariZam_ldu@.gmail.com','15/03/1978')
 
