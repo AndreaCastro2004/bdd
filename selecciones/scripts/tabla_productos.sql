5fc9cee8e6e0b0a8d1c804597a79265c2f936faa
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-18 08:37:41 -0500

    Se crea carpeta_selecciones con scripts

---
+++ selecciones/scripts/tabla_productos.sql
@@ -0,0 +1,40 @@
+--crear una tabla llamada productos
+drop table productos
+create table productos(
+	codigo int not null,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	precio money not null,
+	stock int not null,
+	constraint productos_pk primary key(codigo)
+)
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values('123455','leche','Aporta vitaminas A, B y C',1.00,5)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values('098767','yogurt','realizada con frutas de la mejor calidad',1.37,8)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values('768590','vino','Añejada desde 1998',5,9)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values('958695','Galletas','Mantiene el toque especial',1.25,3)
+
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values('000012','Rosquitas','Lo mejor para picar en tu tiempo libre',1.34,12)
+
+
+--se ejecutan solo los campos obligatorios
+insert into productos (codigo,nombre,precio,stock)
+values('019823','Calamar',7,9)
+
+insert into productos (codigo,nombre,precio,stock)
+values('657433','Camaron',3.98,4)
+
+insert into productos (codigo,nombre,precio,stock)
+values('090767','Carne',3.00,7)
+
+
+
+
+
