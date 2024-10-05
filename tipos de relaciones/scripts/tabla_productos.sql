ebddb4d3a475ebf27fb4de4cb84faa7f72d0661d
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-10-05 16:13:05 -0500

    tabla productos tabla ventas

---
+++ tipos de relaciones/scripts/tabla_productos.sql
@@ -0,0 +1,40 @@
+--crear una tabla llamada productos
+drop table productos
+create table productos(
+	codigo int not null,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	precio money not null,
+	stock int not null,
+	constraint productos_pk primary key(codigo)
+)
+
+
+
+
+insert into productos(codigo,nombre,precio,stock) 
+values (12,'Mantel',3.50,10);
+insert into productos(codigo,nombre,descripcion,precio,stock) 
+values (13,'Mariscos',2.50,20);
+insert into productos(codigo,nombre,descripcion,precio,stock) 
+values (14,'Mantequilla',1.50,30);
+insert into productos(codigo,nombre,descripcion,precio,stock) 
+values (15,'Mayonesa','Fortident',3.0,40);
+insert into productos(codigo,nombre,descripcion,precio,stock) 
+values (16,'Manteca','Para las comidas',2.50,50);
+insert into productos(codigo,nombre,precio,stock) 
+values (17,'Molino',1.50,50);
+insert into productos(codigo,nombre,precio,stock) 
+values (18,'Queso',5.50,60);
+insert into productos(codigo,nombre,precio,stock)
+values (19,'Leche',2.50,70);
+insert into productos(codigo,nombre,precio,stock)
+values (20,'Jamon',1.00,120);
+insert into productos(codigo,nombre,precio,stock)
+values (21,'Coca Cola',3.50,300);
+
+
+
+
+
+
---
+++ tipos de relaciones/scripts/tabla_ventas.sql
@@ -0,0 +1,36 @@
+drop table ventas
+create table ventas(
+	id_venta int,
+	codigo_producto int not null,
+	fecha_venta date not null,
+	cantidad int,
+	constraint ventas_pk primary key (id_venta)
+)
+--referenciar table
+alter table productos
+add constraint productos_ventas_fk foreign key (codigo)
+references ventas(codigo_producto)
+select * from ventas
+select * from productos
+--10 insert
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('1','12','2023-09-12','5');
+insert into ventas (id_venta,codigo_producto,fecha_venta,cantidad)
+values ('2','13','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('3','14','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('4','15','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('5','16','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('6','17','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('7','18','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('8','19','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('9','20','2024-09-12','10');
+insert into ventas(id_venta,codigo_producto,fecha_venta,cantidad)
+values('10','21','2024-09-12','10')
+
