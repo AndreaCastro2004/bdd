f47d0baa26e8566f26344f9d9f4b9961a765dd32
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-10-06 15:00:12 -0500

    modifico videojuegos

--- consultar Tablas relacionadas/scripts/tabla_videojuegos.sql
+++ consultar Tablas relacionadas/scripts/tabla_videojuegos.sql
@@ -15,11 +15,11 @@ references videojuegos (codigo)
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
 values(123, 'Call of Duty', 'Guerra', 9);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values(1234, 'Free Fire', 'guerra', 7);
+values(1234, 'Free Fire', 'Guerra', 7);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
 values(1235, 'Call of Clans', 'juego de terror', 10);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values(1236, 'Fornite', 'guerra', 10);
+values(1236, 'Fornite', 'Guerra', 10);
 insert into videojuegos(codigo, nombre, valoracion)
 values(1237, 'Minecraft', 3);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
@@ -27,21 +27,22 @@ values(1238, 'Tom and Jerry', 'juego de comedia', 8);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
 values(1239, 'Ajedres', 'juego de destreza', 6);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values(1231, 'God of Ward', 'guerra', 7);--codigo 
+values(1231, 'God of Ward', 'Guerra', 7);--codigo 
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
 values(122, 'Crash', 'juego de deprote', 9);
 insert into videojuegos(codigo, nombre, descripcion, valoracion)
-values(121, 'Dragon Ball', 'guerra', 8);
+values(121, 'Dragon Ball', 'Guerra', 9);
 
 
 select * from videojuegos
 select * from plataformas
 --consulta
-select vi.*,pla.* from 
+select vi.nombre,vi.descripcion,vi.valoracion,pla.nombre_plataforma from 
 videojuegos vi, plataformas pla
 where vi.codigo = pla.codigo_videojuego
-and descripcion ='guerra'
-or (valoracion>7 and nombre like 'C%') or (valoracion>8 and nombre like 'D')
+and descripcion ='Guerra'
+and (vi.valoracion>7 and vi.nombre like 'C%')
+or (vi.valoracion>8 and vi.nombre like 'D%');
 --subconsulta
 select pla.* from 
 videojuegos vi, plataformas pla
