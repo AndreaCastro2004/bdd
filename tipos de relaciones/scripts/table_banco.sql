d5a310e0e271fff8fe94edda38b3c8df904722e9
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-10-05 22:43:46 -0500

    guardo tabla banco

--- tipos de relaciones/scripts/table_banco.sql
+++ tipos de relaciones/scripts/table_banco.sql
@@ -4,3 +4,25 @@ create table banco(
 	detalle varchar(100),
 	constraint banco_pk primary key (codigo_banco)
 )
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(0,1,'movimiento de una cantidad de 100');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(23,1,'movimiento de una cantidad de 100');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(1,2,'movimiento de una cantidad de 120');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(2,3,'movimiento de una cantidad de 110');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(3,4,'movimiento de una cantidad de 120');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(4,5,'movimiento de una cantidad de 110');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(5,6,'movimiento de una cantidad de 130');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(6,7,'movimiento de una cantidad de 110');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(7,8,'movimiento de una cantidad de 130');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(8,9,'movimiento de una cantidad de 120');
+insert into banco (codigo_banco,codigo_transaccion,detalle)
+values(9,10,'movimiento de una cantidad de 120');
